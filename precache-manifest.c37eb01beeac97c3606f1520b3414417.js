4e78777f4dcd196f1ef9e272901e5fa6d34f46d8
haseebasimm <haseebzaidi123@gmail.com>
2021-07-13 11:44:11 +0500

    Updates

---
+++ precache-manifest.c37eb01beeac97c3606f1520b3414417.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154379c089c6ab072c8aa171fe1ac43a",
+    "url": "/Parho-Pakistan-Front-End/index.html"
+  },
+  {
+    "revision": "995ee9ef4cf3612eea20",
+    "url": "/Parho-Pakistan-Front-End/static/css/main.963a45c6.chunk.css"
+  },
+  {
+    "revision": "42b699f5acc6916a46e9",
+    "url": "/Parho-Pakistan-Front-End/static/js/2.8497ad11.chunk.js"
+  },
+  {
+    "revision": "058fe86379bda061c55c17c08c87bc4a",
+    "url": "/Parho-Pakistan-Front-End/static/js/2.8497ad11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "995ee9ef4cf3612eea20",
+    "url": "/Parho-Pakistan-Front-End/static/js/main.1092d75e.chunk.js"
+  },
+  {
+    "revision": "5fdb6b77bf2ecf6fa299",
+    "url": "/Parho-Pakistan-Front-End/static/js/runtime-main.b34fc9f9.js"
+  },
+  {
+    "revision": "8b731dee5d6d342b813b86f43dcefebe",
+    "url": "/Parho-Pakistan-Front-End/static/media/HEC.8b731dee.png"
+  },
+  {
+    "revision": "3bda8bcde1504055ccb44d4b1b90f836",
+    "url": "/Parho-Pakistan-Front-End/static/media/aboutcontentimg.3bda8bcd.png"
+  },
+  {
+    "revision": "0329141e8eb6939e7a0f49ea6d32d395",
+    "url": "/Parho-Pakistan-Front-End/static/media/aboutheaderimg.0329141e.png"
+  },
+  {
+    "revision": "06f70ee0d2f4d122292aea060ddcd853",
+    "url": "/Parho-Pakistan-Front-End/static/media/aboutsignupimg.06f70ee0.png"
+  },
+  {
+    "revision": "71fdfc6e03751526af2b739aa9d8b595",
+    "url": "/Parho-Pakistan-Front-End/static/media/charitylife.71fdfc6e.png"
+  },
+  {
+    "revision": "5c4d831f730eab0c3559d5879e74033e",
+    "url": "/Parho-Pakistan-Front-End/static/media/childprofilebg.5c4d831f.svg"
+  },
+  {
+    "revision": "a7c5cad98ea996cbc28bbf0eff6b4621",
+    "url": "/Parho-Pakistan-Front-End/static/media/nationalfood.a7c5cad9.png"
+  },
+  {
+    "revision": "f22b291d9e419ae8411f1a15640bf6e6",
+    "url": "/Parho-Pakistan-Front-End/static/media/navimg.f22b291d.png"
+  },
+  {
+    "revision": "6e2c9de1bbc1efa4bbbc8fc9222140ff",
+    "url": "/Parho-Pakistan-Front-End/static/media/pakmap.6e2c9de1.png"
+  },
+  {
+    "revision": "740e35be8f8669d6fceed30e73e5c1e2",
+    "url": "/Parho-Pakistan-Front-End/static/media/redbull.740e35be.png"
+  },
+  {
+    "revision": "1b18e8b2d4b1b281ad17141b260eb650",
+    "url": "/Parho-Pakistan-Front-End/static/media/sadboy.1b18e8b2.png"
+  },
+  {
+    "revision": "fcd83e67d1cf21a979c830fc1eea2a13",
+    "url": "/Parho-Pakistan-Front-End/static/media/sponsor_comp_header.fcd83e67.png"
+  },
+  {
+    "revision": "baed3f99fcc4d0fde55e2b91ff8abb4f",
+    "url": "/Parho-Pakistan-Front-End/static/media/sponsorcardimg.baed3f99.png"
+  },
+  {
+    "revision": "eef2863e51a44f992d044b5891f23b75",
+    "url": "/Parho-Pakistan-Front-End/static/media/students.eef2863e.png"
+  },
+  {
+    "revision": "709e5b04446520d44e8b363df4441d97",
+    "url": "/Parho-Pakistan-Front-End/static/media/successstory.709e5b04.png"
+  },
+  {
+    "revision": "e67ff2be68cf3be06bb5c7b5dd458a30",
+    "url": "/Parho-Pakistan-Front-End/static/media/yannis-h-uaPaEM7MiQQ-unsplash.e67ff2be.png"
+  }
+]);
